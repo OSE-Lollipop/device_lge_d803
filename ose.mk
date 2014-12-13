ec89dcc1f0919e5a1c41cc0131ea1bbb0720c0d6
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-12-13 09:11:55 -0500

    Update and rename slim.mk to ose.mk

--- ose.mk
+++ ose.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := d803
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common OSE stuff.
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/d803/full_d803.mk)
+$(call inherit-product, device/ose/d803/full_d803.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d803
-PRODUCT_NAME := slim_d803
+PRODUCT_NAME := ose_d803
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D803
 PRODUCT_MANUFACTURER := lge
